1ebafdef4df180b30550471c0916730eded93ac4
Samy <dev.exelight@gmail.com>
2022-02-11 17:54:17 +0100

    Fetch Movies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [popular, setPopular] = useState([]);
+  
+  const fetchPopular = async () => {
+    const response = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=1af8f5a0dac921ed793eaf9b1a89b23e&language=en-US&page=1");
+    const movies = await response.json();
+    setPopular(movies.results);
+    console.log(movies.results);
+  }
+
+  useEffect(() => {
+    fetchPopular();
+  }, []);
+  
+
+
   return (
     <div className="App">
       <h1>Hello</h1>
